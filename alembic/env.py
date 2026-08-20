1dfe40158b59218bb0902b577d73fbcccb1231c7
djumanov <djumanovdev@gmail.com>
2026-08-20 20:15:01 +0500

    feat: events model

--- alembic/env.py
+++ alembic/env.py
@@ -12,6 +12,7 @@ from src.users.models import User  # noqa: F401
 from src.organizers.models import Organizer  # noqa: F401
 from src.venues.models import Venue  # noqa: F401
 from src.categories.models import Category  # noqa: F401
+from src.events.models import Event  # noqa: F401
 
 # Domain modellari shu yerda import qilinadi — shundagina Alembic ularni
 # `Base.metadata`ga ro'yxatdan o'tgan deb ko'radi va autogenerate ishlaydi
